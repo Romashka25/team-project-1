47e84c1490e6624174a9fdcac005d239e771e671
Svitlana Kovalchuk <svetikkovalchuk25@gmail.com>
2023-07-07 15:21:46 +0300

    fix code

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,22 @@
-const burger = document.querySelector(".nav-burger");
-const navList = document.querySelector(".nav-list");
+const burger = document.querySelector(".header__burger");
+const nav = document.querySelector(".header__nav");
 
-burger.addEventListener("click", function () {
-  const prevActive = burger.querySelector(".show");
-  const nextActive = burger.querySelector(".hide");
-  prevActive.classList.replace("show", "hide");
-  nextActive.classList.replace("hide", "show");
-  navList.classList.toggle("active");
+burger.addEventListener("click", function (event) {
+  burger.classList.toggle("header__burger--active");
+  nav.classList.toggle("header__nav--active");
+  event.clickBurger = true;
+});
+
+document.body.addEventListener("click", function (event) {
+  if (
+    event.clickBurger === true ||
+    event.target.classList.contains(
+      "header__nav-link",
+      "header__nav-list-item",
+      "header__nav"
+    )
+  )
+    return;
+  burger.classList.remove("header__burger--active");
+  nav.classList.remove("header__nav--active");
 });
